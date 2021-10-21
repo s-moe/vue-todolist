344cfc0df83cd9022e99fccfb0f9c10d93f34697
s-moe <sarahannmoe@gmail.com>
2021-10-21 12:41:52 -0700

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <img src="@/assests/logo.png" alt="Vue Logo" id="vue-logo" />
+    <div class="title">What do I need to do today?</div>
+    <input v-model="myTodo" /><button @click="addToDo">Add</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function() {
+    return {
+      myTodo: "",
+    };
+  },
+  methods: {
+    addToDo: function() {
+      console.log("my todo: " + this.myTodo);
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body,
+html,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #8ac8e5;
+}
+.home {
+  width: 300px;
+  margin: auto;
+}
+
+#vue-logo {
+  width: 100px;
+}
+input,
+button {
+  border: 0;
+  width: 100%;
+  margin: 0 0 10px;
+  padding: 7px;
+}
+input {
+  font-size: 12px;
+}
+button {
+  background: #43b823;
+  border: 0;
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: 700;
+  cursor: pointer;
+}
+.title {
+  font-size: 14px;
+  font-weight: 700;
+  padding: 0 0 10px 0;
+  margin: 0 0 10px 0;
+  border-bottom: 1px solid #666;
+}
+#errors {
+  background: #a52222;
+  color: #fff;
+  padding: 5px;
 }
 </style>
